0a9f68c212d972890e565c7172b31dffe669d669
DeveloperSantiago <minipochis48@gmail.com>
2024-12-28 18:38:03 -0500

    Agregar archivos de proyecto.

---
+++ BookingPro/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Mvc;
+using BookingPro.Data;
+using Dapper;
+
+namespace BookingPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DbContext dbContext;
+
+        public HomeController()
+        {
+            dbContext = new DbContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestConnection()
+        {
+            string message;
+
+            try
+            {
+                using (var connection = dbContext.CreateConnection())
+                {
+                    connection.Open();
+                    var query = "SELECT 1";
+                    var result = connection.ExecuteScalar(query);
+                    message = result != null ? "Connection successful!" : "Connection failed!";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = $"Connection failed: {ex.Message}";
+            }
+
+            ViewBag.Message = message;
+            return View();
+        }
+    }
+}
---
+++ BookingPro/Data/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BookingPro.Data
+{
+    public class DbContext
+    {
+        private readonly string connectionString;
+
+        public DbContext()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
